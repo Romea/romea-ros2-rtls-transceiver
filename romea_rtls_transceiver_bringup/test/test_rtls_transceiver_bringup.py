f6572d809dc49ab5eb359ec95662bab6ced2b265
jean.laneurit <jean.laneurit@inrae.fr>
2023-05-02 10:40:14 +0200

    add license Signed-off-by: jean.laneurit <jean.laneurit@inrae.fr>

--- romea_rtls_transceiver_bringup/test/test_rtls_transceiver_bringup.py
+++ romea_rtls_transceiver_bringup/test/test_rtls_transceiver_bringup.py
@@ -1,5 +1,17 @@
 # Copyright 2022 INRAE, French National Research Institute for Agriculture, Food and Environment
-# Add license
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 
 
 import os
@@ -8,7 +20,8 @@ import pytest
 from romea_rtls_transceiver_bringup import RTLSTransceiverMetaDescription
 
 
-@pytest.fixture(scope="module")
+
+pytest.fixture(scope="module")
 def meta_description():
     meta_description_file_path = os.path.join(os.getcwd(), "test_rtls_transceiver_bringup.yaml")
     return RTLSTransceiverMetaDescription(meta_description_file_path)
